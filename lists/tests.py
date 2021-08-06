ada71ac7b91fe94f8f761815bb5a914e3ea0b32e
orudda <othavio.rudda.cc@gmail.com>
2021-08-06 18:47:50 -0300

    Processando Requisição POST

--- lists/tests.py
+++ lists/tests.py
@@ -10,12 +10,16 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')  
+    # def test_home_page_returns_correct_html(self):
+    #     response = self.client.get('/')  
 
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    #     html = response.content.decode('utf8')  
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html') 
+    #     self.assertTemplateUsed(response, 'home.html') 
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
